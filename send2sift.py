5ad092691a0221e7b044e19f8fa4e0b37c7b0f13
jakoraptor <77681812+jakoraptor@users.noreply.github.com>
2021-04-01 10:59:48 +0100

    Change from Responder to Analyzer
    Whilst testing it was found that the "responder" was looking at the case artefact not the file itself so was unable to send the file. This was remedied by changing the cortexutils function to Analyzer instead, so it now runs as an Analyzer.

--- send2sift.py
+++ send2sift.py
@@ -1,45 +1,29 @@
 #!/usr/bin/python3
 """Perform tasks against a remote host."""
-from cortexutils.responder import Responder
+from cortexutils.analyzer import Analyzer
 from client import RemoteClient
 from os import walk, path
 from typing import List
 
-local_file_directory = "/tmp/bloops/"
 
-
-class SendToSift(Responder):
+class SendToSift(Analyzer):
     def __init__(self):
-        Responder.__init__(self)
-        self.host = self.get_param("config.host", "192.168.135.133", "no host provided")
+        Analyzer.__init__(self)
+        self.host = self.get_param("config.host", None, "no host provided")
         self.username = self.get_param('config.username', None, 'Remote host username is missing')
         # self.password = self.get_param('config.password', None, None)
         self.ssh_key_filepath = self.get_param('config.ssh_key_filepath', None, 'Location of local SSH key required')
-        self.remote_path = self.get_param('config.remote_path', '/home/sansforensics/suspect', 'Remote path location required')
+        self.remote_path = self.get_param('config.remote_path', None, 'Remote path location required')
 
     def run(self):
-        Responder.run(self)
+        Analyzer.run(self)
         if self.data_type == "file":
             """Initialise remote host client and execute actions."""
+            filepath = self.get_param('file', None, 'file is missing')
             remote = RemoteClient(self.host, self.username, self.ssh_key_filepath, self.remote_path)
-            upload_files_to_remote(remote)
+            remote.bulkupload(filepath)
         else:
             self.error("invalid data type!")
-
-
-def upload_files_to_remote(remote) -> object:
-    """Upload bloops to remote via SCP"""
-    local_files = fetch_local_files(local_file_directory)
-    remote.bulk_upload(local_files)
-
-
-def fetch_local_files(local_file_dir: str) -> List[str]:
-    """Create list of file paths."""
-    local_files = walk(local_file_dir)
-    basedir = path.abspath(path.dirname(__file__))
-    for root, dirs, files in local_files:
-        return [f"{basedir}/{root}/{file}" for file in files]
-
 
 if __name__ == "__main__":
     SendToSift().run()
