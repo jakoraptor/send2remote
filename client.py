64ec43e9f214e70d6d5114a8ebc3678e5fd30284
jakoraptor <77681812+jakoraptor@users.noreply.github.com>
2021-04-22 11:50:05 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 from paramiko import SSHClient, AutoAddPolicy, RSAKey
 from paramiko.auth_handler import AuthenticationException, SSHException
 from scp import SCPClient
